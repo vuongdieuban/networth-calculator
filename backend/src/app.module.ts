aa96b7eac29715d675ac1cb96a8c22468267b7f8
Ban Vuong <vuongdieuban@gmail.com>
2021-11-06 17:54:13 -0600

    add healthcheck

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
+import { HealthController } from './health/health.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
-  controllers: [],
+  imports: [TypeOrmModule.forRoot(), TerminusModule, HttpModule],
+  controllers: [HealthController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
