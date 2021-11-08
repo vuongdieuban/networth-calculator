47c01287f238f0d7e84cb5299f74cb3d49c60afe
Ban Vuong <vuongdieuban@gmail.com>
2021-11-07 19:26:01 -0700

    change response type

--- backend/src/networth/dtos/networth-view-response.ts
+++ backend/src/networth/dtos/networth-view-response.ts
@@ -3,42 +3,34 @@ import { CurrencyType } from 'src/shared/constants/currency-type.enum';
 export class NetworthViewResponseDto {
   selectedCurrency: CurrencyType;
   totalNetworth: number;
+  totalAssets: number;
+  totalLiabilities: number;
   assets: {
-    totalAssets: number;
-    cashAndInvestments: {
-      chequing: number;
-      savingsForTaxes: number;
-      rainyDayFund: number;
-      savingsForFun: number;
-      savingsForTravel: number;
-      savingsForPersonalDevelopment: number;
-      investment1: number;
-      investment2: number;
-      investment3: number;
-    };
-    longTermAssets: {
-      primaryHome: number;
-      secondaryHome: number;
-      other: number;
-    };
+    chequing: number;
+    savingsForTaxes: number;
+    rainyDayFund: number;
+    savingsForFun: number;
+    savingsForTravel: number;
+    savingsForPersonalDevelopment: number;
+    investment1: number;
+    investment2: number;
+    investment3: number;
+    primaryHome: number;
+    secondaryHome: number;
+    other: number;
   };
   liabilities: {
-    totalLiabilities: number;
-    shortTermLiabilities: {
-      creditCard1: number;
-      creditCard1MonthlyPayment: number;
-      creditCard2: number;
-      creditCard2MonthlyPayment: number;
-    };
-    longTermDebt: {
-      mortgage1: number;
-      mortgage1MonthlyPayment: number;
-      mortgage2: number;
-      mortgage2MonthlyPayment: number;
-      lineOfCredit: number;
-      lineOfCreditMonthlyPayment: number;
-      investmentLoan: number;
-      investmentLoanMonthlyPayment: number;
-    };
+    creditCard1: number;
+    creditCard1MonthlyPayment: number;
+    creditCard2: number;
+    creditCard2MonthlyPayment: number;
+    mortgage1: number;
+    mortgage1MonthlyPayment: number;
+    mortgage2: number;
+    mortgage2MonthlyPayment: number;
+    lineOfCredit: number;
+    lineOfCreditMonthlyPayment: number;
+    investmentLoan: number;
+    investmentLoanMonthlyPayment: number;
   };
 }
