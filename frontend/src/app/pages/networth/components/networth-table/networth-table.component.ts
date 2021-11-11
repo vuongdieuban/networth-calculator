94c995042bd5013d7216caea0b8cbfdd905c1d46
Ban Vuong <vuongdieuban@gmail.com>
2021-11-10 20:41:43 -0700

    able to use ngFor on form

--- frontend/src/app/pages/networth/components/networth-table/networth-table.component.ts
+++ frontend/src/app/pages/networth/components/networth-table/networth-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CurrencyType } from '../../dtos/networth-data.interface';
 
 @Component({
@@ -9,5 +10,25 @@ import { CurrencyType } from '../../dtos/networth-data.interface';
 export class NetworthTableComponent implements OnInit {
   // should fetch currencies from backend, remove currency type enum.
   currencies = Object.values(CurrencyType) as string[];
+  formFieldValidators = [
+    Validators.required,
+    Validators.pattern(/^[1-9]\d*(((,\d{3}){1})?(\.\d{0,2})?)$/),
+  ];
+
+  public form = new FormGroup({
+    chequing: new FormControl('', this.formFieldValidators),
+    savingsForTaxes: new FormControl('', this.formFieldValidators),
+  });
+
+  networthData = [
+    { label: 'Chequing', formName: 'chequing' },
+    { label: 'Savings For Taxes', formName: 'savingsForTaxes' },
+  ];
+
   ngOnInit() {}
+
+  onSubmit() {
+    console.log('Chequing', this.form.get('chequing')?.value);
+    console.log('FORM VALUES', this.form.value);
+  }
 }
