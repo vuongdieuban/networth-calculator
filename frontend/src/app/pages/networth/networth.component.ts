67ef2365361a45573837905f1df77c625c53a4dd
Ban Vuong <vuongdieuban@gmail.com>
2021-11-12 14:26:01 -0700

    add logout button

--- frontend/src/app/pages/networth/networth.component.ts
+++ frontend/src/app/pages/networth/networth.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/service/auth.service';
 
 @Component({
   selector: 'app-networth',
   templateUrl: './networth.component.html',
-  styleUrls: ['./networth.component.scss']
+  styleUrls: ['./networth.component.scss'],
 })
 export class NetworthComponent implements OnInit {
+  constructor(private readonly authService: AuthService, private readonly router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public handleLogoutClick() {
+    this.authService.logout().subscribe(() => {
+      console.log('go back to login page');
+      this.router.navigate(['login']);
+    });
   }
-
 }
