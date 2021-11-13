00672aa847a0e637f4d722d946ed147fb22d7f1b
Ban Vuong <vuongdieuban@gmail.com>
2021-11-12 18:42:49 -0700

    refactor networth view response dto

--- backend/src/networth/dtos/networth-view-response.ts
+++ backend/src/networth/dtos/networth-view-response.ts
@@ -1,40 +1,36 @@
 import { CurrencyType } from 'src/currency/constants/currency-type.enum';
 
-export class ViewAssetCategories {
-  chequing: string;
-  savingsForTaxes: string;
-  rainyDayFund: string;
-  savingsForFun: string;
-  savingsForTravel: string;
-  savingsForPersonalDevelopment: string;
-  investment1: string;
-  investment2: string;
-  investment3: string;
-  primaryHome: string;
-  secondaryHome: string;
-  other: string;
-}
-
-export class ViewLiabilityCategories {
-  creditCard1: string;
-  creditCard1MonthlyPayment: string;
-  creditCard2: string;
-  creditCard2MonthlyPayment: string;
-  mortgage1: string;
-  mortgage1MonthlyPayment: string;
-  mortgage2: string;
-  mortgage2MonthlyPayment: string;
-  lineOfCredit: string;
-  lineOfCreditMonthlyPayment: string;
-  investmentLoan: string;
-  investmentLoanMonthlyPayment: string;
-}
-
 export class NetworthViewResponseDto {
   selectedCurrency: CurrencyType;
   totalNetworth: string;
   totalLiabilities: string;
   totalAssets: string;
-  assets: ViewAssetCategories;
-  liabilities: ViewLiabilityCategories;
+  assets: {
+    chequing: string;
+    savingsForTaxes: string;
+    rainyDayFund: string;
+    savingsForFun: string;
+    savingsForTravel: string;
+    savingsForPersonalDevelopment: string;
+    investment1: string;
+    investment2: string;
+    investment3: string;
+    primaryHome: string;
+    secondaryHome: string;
+    other: string;
+  };
+  liabilities: {
+    creditCard1: string;
+    creditCard1MonthlyPayment: string;
+    creditCard2: string;
+    creditCard2MonthlyPayment: string;
+    mortgage1: string;
+    mortgage1MonthlyPayment: string;
+    mortgage2: string;
+    mortgage2MonthlyPayment: string;
+    lineOfCredit: string;
+    lineOfCreditMonthlyPayment: string;
+    investmentLoan: string;
+    investmentLoanMonthlyPayment: string;
+  };
 }
