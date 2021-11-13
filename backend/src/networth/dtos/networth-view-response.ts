565adc292791c8a9f0472416cb16c1e256451ef2
Ban Vuong <vuongdieuban@gmail.com>
2021-11-12 18:37:32 -0700

    refactor networth response dto and its adapter

--- backend/src/networth/dtos/networth-view-response.ts
+++ backend/src/networth/dtos/networth-view-response.ts
@@ -1,26 +1,33 @@
 import { CurrencyType } from 'src/currency/constants/currency-type.enum';
 
-export class ViewAssetTypeDetails {
-  label: string;
-  fieldName: string;
-  amount: string; // for display purpose "5.00"
-}
-
 export class ViewAssetCategories {
-  cashAndInvestments: ViewAssetTypeDetails[];
-  longTermAssets: ViewAssetTypeDetails[];
-}
-
-export class ViewLiabilityTypeDetails {
-  label: string;
-  fieldName: string;
-  amount: string;
-  monthlyPayment: string;
+  chequing: string;
+  savingsForTaxes: string;
+  rainyDayFund: string;
+  savingsForFun: string;
+  savingsForTravel: string;
+  savingsForPersonalDevelopment: string;
+  investment1: string;
+  investment2: string;
+  investment3: string;
+  primaryHome: string;
+  secondaryHome: string;
+  other: string;
 }
 
 export class ViewLiabilityCategories {
-  shortermLiabilities: ViewLiabilityTypeDetails[];
-  longTermDebts: ViewLiabilityTypeDetails[];
+  creditCard1: string;
+  creditCard1MonthlyPayment: string;
+  creditCard2: string;
+  creditCard2MonthlyPayment: string;
+  mortgage1: string;
+  mortgage1MonthlyPayment: string;
+  mortgage2: string;
+  mortgage2MonthlyPayment: string;
+  lineOfCredit: string;
+  lineOfCreditMonthlyPayment: string;
+  investmentLoan: string;
+  investmentLoanMonthlyPayment: string;
 }
 
 export class NetworthViewResponseDto {
--- backend/src/networth/services/view-adapter/view-adapter.service.ts
+++ backend/src/networth/services/view-adapter/view-adapter.service.ts
@@ -10,123 +10,55 @@ export class ViewAdapterService {
 
     return {
       selectedCurrency,
-      totalNetworth: this.formatNumberToDisplayString(totalNetworth),
-      totalAssets: this.formatNumberToDisplayString(totalAssets),
-      totalLiabilities: this.formatNumberToDisplayString(totalLiabilities),
+      totalNetworth: this.convertNumberToDisplayString(totalNetworth),
+      totalAssets: this.convertNumberToDisplayString(totalAssets),
+      totalLiabilities: this.convertNumberToDisplayString(totalLiabilities),
       assets: {
-        cashAndInvestments: [
-          {
-            label: 'Chequing',
-            fieldName: 'chequing',
-            amount: this.formatNumberToDisplayString(asset.chequing),
-          },
-          {
-            label: 'Savings For Taxes',
-            fieldName: 'savingsForTaxes',
-            amount: this.formatNumberToDisplayString(asset.savingsForTaxes),
-          },
-          {
-            label: 'Rainy Day Fund',
-            fieldName: 'rainyDayFund',
-            amount: this.formatNumberToDisplayString(asset.rainyDayFund),
-          },
-          {
-            label: 'Savings For Fun',
-            fieldName: 'savingsForFun',
-            amount: this.formatNumberToDisplayString(asset.savingsForFun),
-          },
-          {
-            label: 'Savings For Travel',
-            fieldName: 'savingsForTravel',
-            amount: this.formatNumberToDisplayString(asset.savingsForTravel),
-          },
-          {
-            label: 'Savings For Personal Development',
-            fieldName: 'savingsForPersonalDevelopment',
-            amount: this.formatNumberToDisplayString(asset.savingsForPersonalDevelopment),
-          },
-          {
-            label: 'Investment 1',
-            fieldName: 'investment1',
-            amount: this.formatNumberToDisplayString(asset.investment1),
-          },
-          {
-            label: 'Investment 2',
-            fieldName: 'investment2',
-            amount: this.formatNumberToDisplayString(asset.investment2),
-          },
-          {
-            label: 'Investment 3',
-            fieldName: 'investment3',
-            amount: this.formatNumberToDisplayString(asset.investment3),
-          },
-        ],
-        longTermAssets: [
-          {
-            label: 'Primary Home',
-            fieldName: 'primaryHome',
-            amount: this.formatNumberToDisplayString(asset.primaryHome),
-          },
-          {
-            label: 'Secondary Home',
-            fieldName: 'secondaryHome',
-            amount: this.formatNumberToDisplayString(asset.secondaryHome),
-          },
-          {
-            label: 'Other',
-            fieldName: 'other',
-            amount: this.formatNumberToDisplayString(asset.other),
-          },
-        ],
+        chequing: this.convertNumberToDisplayString(asset.chequing),
+        savingsForTaxes: this.convertNumberToDisplayString(asset.savingsForTaxes),
+        rainyDayFund: this.convertNumberToDisplayString(asset.rainyDayFund),
+        savingsForFun: this.convertNumberToDisplayString(asset.savingsForFun),
+        savingsForTravel: this.convertNumberToDisplayString(asset.savingsForTravel),
+        savingsForPersonalDevelopment: this.convertNumberToDisplayString(
+          asset.savingsForPersonalDevelopment,
+        ),
+        investment1: this.convertNumberToDisplayString(asset.investment1),
+        investment2: this.convertNumberToDisplayString(asset.investment2),
+        investment3: this.convertNumberToDisplayString(asset.investment3),
+        primaryHome: this.convertNumberToDisplayString(asset.primaryHome),
+        secondaryHome: this.convertNumberToDisplayString(asset.secondaryHome),
+        other: this.convertNumberToDisplayString(asset.other),
       },
       liabilities: {
-        shortermLiabilities: [
-          {
-            label: 'Credit Card 1',
-            fieldName: 'creditCard1',
-            amount: this.formatNumberToDisplayString(liability.creditCard1),
-            monthlyPayment: this.formatNumberToDisplayString(liability.creditCard1MonthlyPayment),
-          },
-          {
-            label: 'Credit Card 2',
-            fieldName: 'creditCard2',
-            amount: this.formatNumberToDisplayString(liability.creditCard2),
-            monthlyPayment: this.formatNumberToDisplayString(liability.creditCard2MonthlyPayment),
-          },
-        ],
-        longTermDebts: [
-          {
-            label: 'Mortgage 1',
-            fieldName: 'mortgage1',
-            amount: this.formatNumberToDisplayString(liability.mortgage1),
-            monthlyPayment: this.formatNumberToDisplayString(liability.mortgage1MonthlyPayment),
-          },
-          {
-            label: 'Mortgage 2',
-            fieldName: 'mortgage2',
-            amount: this.formatNumberToDisplayString(liability.mortgage2),
-            monthlyPayment: this.formatNumberToDisplayString(liability.mortgage2MonthlyPayment),
-          },
-          {
-            label: 'Line Of Credit',
-            fieldName: 'lineOfCredit',
-            amount: this.formatNumberToDisplayString(liability.lineOfCredit),
-            monthlyPayment: this.formatNumberToDisplayString(liability.lineOfCreditMonthlyPayment),
-          },
-          {
-            label: 'Investment Loan',
-            fieldName: 'investmentLoan',
-            amount: this.formatNumberToDisplayString(liability.investmentLoan),
-            monthlyPayment: this.formatNumberToDisplayString(
-              liability.investmentLoanMonthlyPayment,
-            ),
-          },
-        ],
+        creditCard1: this.convertNumberToDisplayString(liability.creditCard1),
+        creditCard1MonthlyPayment: this.convertNumberToDisplayString(
+          liability.creditCard1MonthlyPayment,
+        ),
+        creditCard2: this.convertNumberToDisplayString(liability.creditCard2),
+        creditCard2MonthlyPayment: this.convertNumberToDisplayString(
+          liability.creditCard2MonthlyPayment,
+        ),
+        mortgage1: this.convertNumberToDisplayString(liability.mortgage1),
+        mortgage1MonthlyPayment: this.convertNumberToDisplayString(
+          liability.mortgage1MonthlyPayment,
+        ),
+        mortgage2: this.convertNumberToDisplayString(liability.mortgage2),
+        mortgage2MonthlyPayment: this.convertNumberToDisplayString(
+          liability.mortgage2MonthlyPayment,
+        ),
+        lineOfCredit: this.convertNumberToDisplayString(liability.lineOfCredit),
+        lineOfCreditMonthlyPayment: this.convertNumberToDisplayString(
+          liability.lineOfCreditMonthlyPayment,
+        ),
+        investmentLoan: this.convertNumberToDisplayString(liability.investmentLoan),
+        investmentLoanMonthlyPayment: this.convertNumberToDisplayString(
+          liability.investmentLoanMonthlyPayment,
+        ),
       },
     };
   }
 
-  private formatNumberToDisplayString(amount: number): string {
+  private convertNumberToDisplayString(amount: number): string {
     return amount.toFixed(2);
   }
 }
