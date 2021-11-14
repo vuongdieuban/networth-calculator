a9583ab8920c5a41b0f4552e1e1f86641caf6227
Ban Vuong <vuongdieuban@gmail.com>
2021-11-13 23:56:31 -0700

    asset and liability service test

--- backend/src/networth/services/asset/asset.service.spec.ts
+++ backend/src/networth/services/asset/asset.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { AssetInput } from 'src/networth/dtos/calculate-networth-request.dto';
+import { AssetEntity } from 'src/networth/entities/asset.entity';
+import { MockType, repositoryMockFactory } from 'src/shared/utils/test-utils';
+import { Repository } from 'typeorm';
 import { AssetService } from './asset.service';
 
 describe('AssetService', () => {
   let service: AssetService;
+  let assetRepo: MockType<Repository<AssetEntity>>;
+  const userIdMock = 'abc';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AssetService],
+      providers: [
+        AssetService,
+        { provide: getRepositoryToken(AssetEntity), useFactory: repositoryMockFactory },
+      ],
     }).compile();
 
     service = module.get<AssetService>(AssetService);
+    assetRepo = module.get(getRepositoryToken(AssetEntity));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('asset update and rate calculation', () => {
+    it('should return undefined if cannot find asset profile by userId', async () => {
+      jest.spyOn(assetRepo, 'findOne').mockImplementation(async () => undefined);
+      const result = await service.updateAssetWithNewRateAndInput(userIdMock, 2, {} as AssetInput);
+      expect(result).toBeUndefined();
+    });
+
+    it('should perform multiplication of the assetInput with the rate and save the updated value', async () => {
+      const asset = { chequing: 10 } as AssetEntity;
+      const assetInput = { chequing: 15 } as AssetInput;
+      const rate = 2;
+
+      jest.spyOn(assetRepo, 'findOne').mockImplementation(async () => asset);
+      const saveSpy = jest.spyOn(assetRepo, 'save');
+
+      await service.updateAssetWithNewRateAndInput(userIdMock, rate, assetInput);
+      const savedEntity = saveSpy.mock.calls[0][0] as AssetEntity;
+      expect(savedEntity.chequing).toEqual(assetInput.chequing * rate);
+    });
   });
 });
--- backend/src/networth/services/liability/liability.service.spec.ts
+++ backend/src/networth/services/liability/liability.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { LiabilityInput } from 'src/networth/dtos/calculate-networth-request.dto';
+import { LiabilityEntity } from 'src/networth/entities/liability.entity';
+import { MockType, repositoryMockFactory } from 'src/shared/utils/test-utils';
+import { Repository } from 'typeorm';
 import { LiabilityService } from './liability.service';
 
 describe('LiabilityService', () => {
   let service: LiabilityService;
+  let liabilityRepo: MockType<Repository<LiabilityEntity>>;
+  const userIdMock = 'abc';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LiabilityService],
+      providers: [
+        LiabilityService,
+        { provide: getRepositoryToken(LiabilityEntity), useFactory: repositoryMockFactory },
+      ],
     }).compile();
 
     service = module.get<LiabilityService>(LiabilityService);
+    liabilityRepo = module.get(getRepositoryToken(LiabilityEntity));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('liability update and rate calculation', () => {
+    it('should return undefined if cannot find liability profile by userId', async () => {
+      jest.spyOn(liabilityRepo, 'findOne').mockImplementation(async () => undefined);
+      const result = await service.updateLiabilityWithNewRateAndInput(
+        userIdMock,
+        2,
+        {} as LiabilityInput,
+      );
+      expect(result).toBeUndefined();
+    });
+
+    it('should perform multiplication of the assetInput with the rate and save the updated value', async () => {
+      const liability = { creditCard1: 10 } as LiabilityEntity;
+      const assetInput = { creditCard1: 15 } as LiabilityInput;
+      const rate = 2;
+
+      jest.spyOn(liabilityRepo, 'findOne').mockImplementation(async () => liability);
+      const saveSpy = jest.spyOn(liabilityRepo, 'save');
+
+      await service.updateLiabilityWithNewRateAndInput(userIdMock, rate, assetInput);
+      const savedEntity = saveSpy.mock.calls[0][0] as LiabilityEntity;
+      expect(savedEntity.creditCard1).toEqual(assetInput.creditCard1 * rate);
+    });
   });
 });
